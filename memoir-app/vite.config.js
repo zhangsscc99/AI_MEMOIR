d106b5c2b920a7d8e32d8c0b4e9b350310ab42bd
Yuting Wang <yw4554@nyu.edu>
2025-09-09 18:10:05 +0800

    story png images

--- memoir-app/vite.config.js
+++ memoir-app/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
-import { viteImagemin } from 'vite-plugin-imagemin'
+import viteImagemin from 'vite-plugin-imagemin'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
